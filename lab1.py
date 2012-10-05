7802b22f6f269f05f40ff920680580ed99023800
Tomarrech <tomarech@gmail.com>
2012-10-05 16:05:35 +0400

    rebild

--- lab1.py
+++ lab1.py
@@ -1,12 +1,12 @@
-#coding: 866
+#coding: utf-8
 
 __author__ = 'Issahar'
 
-inLst = raw_input('Введите номера телефонов через запятую: ')
+inLst = raw_input("Input comma separated numbers: ")
 
 numberList = inLst.split(',')
 i = 1
-print '№ \tкод \tномер'
+print '# \tID \tNumber'
 for val in numberList:
     val = eval(val)
     print i,')\t', str(val)[0:3] ,'\t',str(val)[3:] ,'\t'
